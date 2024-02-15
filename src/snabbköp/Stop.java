711d93bb0379bb60911c30dd18c6d343dc020789
Botan Guzel <botanguzel@hotmail.com>
2024-02-15 22:10:55 +0100

    Update Stop.java
    Update the createEvent function so it works properly.

--- src/snabbköp/Stop.java
+++ src/snabbköp/Stop.java
@@ -1,17 +1,22 @@
 package snabbköp;
 
-import generellSim.Event;
-import generellSim.EventQueue;
+import generellSim.*;
+import snabbköp.kunder.*;
+
+import java.util.concurrent.CountDownLatch;
 
 public class Stop extends Event {
-
+	boolean sistaBetalning = false;
 	public Stop(SnabbköpState state, EventQueue eQ, double tid) {
 		super(state, eQ, tid);
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public void createEvent() { state.setSnabbköpÖppet(false); }
+	public void createEvent() {
+		state.setCurrentEvent(this);
+		state.setTime(tid);
+	}
 
 	@Override
 	public String getName() { return "Stänger"; }
