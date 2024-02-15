38769531f44a7df6f2338665a058e5c1c3abb5ee
Botan Guzel <botanguzel@hotmail.com>
2024-02-15 22:05:46 +0100

    Update on körHändelser func
    Update the funciton, so it checks if the market is closed now. If closed then instantiate Stop func and add it to queue.

--- src/generellSim/Sim.java
+++ src/generellSim/Sim.java
@@ -1,12 +1,14 @@
 package generellSim;
 
-import snabbköp.SnabbköpState;
-import snabbköp.SnabbköpView;
+import snabbköp.*;
+import snabbköp.kunder.Betalning;
+import snabbköp.kunder.KundHändelse;
 
 public class Sim {
 	EventQueue eQ;
 	SimState ss;
 	SnabbköpState sss;
+	Object lock = new Object();
 
 	SnabbköpView skV;
 	public Sim(EventQueue eQ, SimState ss, SnabbköpState sss) {
@@ -18,15 +20,30 @@ public class Sim {
 	
 	public void körHändelser() {
 		//skV.update(ss, eQ);
-		while(ss.getIo() && !this.eQ.isEmpty()) {
-			System.out.println(sss.getAntalLedigaKassor());
+		while (ss.getIo() && !this.eQ.isEmpty()) {
+			//System.out.println(String.format("%s\t%s",sss.getAntalLedigaKassor(),  sss.getKassaKöFIFO()));
+			//System.out.println(sss.getAntalLedigaKassor());
 			//for (int i = 0; i < eQ.size(); i++) {System.out.println(eQ.get(i).getName());}
 			eQ.get(0).createEvent();
 			sss.setTime(eQ.get(0).händelseTid());
 			eQ.remove(0);
 			//System.out.println(eQ);
+			if (sss.getTotalAntalKunder() == 6) { sss.setSnabbköpÖppet(false); }
+			if (eQContainsBetalning() && !sss.isSnabbköpÖppet()) {
+				System.out.println(eQ);
+				Stop stop = new Stop(sss, eQ, sss.getTime());
+				eQ.add(1, stop);
+			}
 			skV.update(sss, eQ);
-			if (sss.getTotalAntalKunder() == 5) { sss.setSnabbköpÖppet(false); break;}
 		}
+	}
+
+	private boolean eQContainsBetalning() {
+		for (Event event : eQ) {
+			if (event instanceof Betalning) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
